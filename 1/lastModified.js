16571936473f2d783645aa837485ace0f76b5e64
18716792254 <18716792254@163.com>
2024-11-24 23:00:41 +0800

    Update lastModified.js

--- 1/lastModified.js
+++ 1/lastModified.js
@@ -1,11 +1,5 @@
-// 获取当前网页的最后修改时间
-function displayLastModified() {
-  const lastModified = document.lastModified;  // 获取网页最后修改时间
-  const lastModifiedElement = document.getElementById('last-modified');
-
-  // 将最后修改时间显示在页面上
-  lastModifiedElement.textContent = `网页最后修改时间: ${lastModified}`;
-}
-
-// 调用函数显示网页最后修改时间
-displayLastModified();
+// lastModified.js  
+document.addEventListener('DOMContentLoaded', () => {  
+    const lastModified = document.lastModified;  
+    console.log(`本网页最后更新的时间和日期：${lastModified}`);  
+});
