4efe97c4859f334aa66e1d85651de225d8cf928e
18716792254 <18716792254@163.com>
2024-11-25 00:31:19 +0800

    Add files via upload

---
+++ 1/lastModified.js
@@ -0,0 +1,3 @@
+// lastModified.js  
+const lastModifiedDiv = document.getElementById('last-modified');  
+lastModifiedDiv.innerHTML = `网页最后更新于：${document.lastModified}`;
---
+++ 1/script.js
@@ -0,0 +1,20 @@
+// script.js  
+document.addEventListener('DOMContentLoaded', function() {  
+    const employees = document.querySelectorAll('.employee-card');  
+
+    employees.forEach((employee) => {  
+        const rating = employee.parentElement.getAttribute('data-rating');  
+        employee.addEventListener('click', function() {  
+            const text = employee.innerText; // 获取员工信息  
+            alert(`你点击了：${text} `); // 弹窗显示  
+        });  
+
+        employee.addEventListener('mouseover', function() {  
+            this.style.color = 'blue'; // 鼠标悬停时变色  
+        });  
+
+        employee.addEventListener('mouseout', function() {  
+            this.style.color = ''; // 鼠标移出时恢复颜色  
+        });  
+    });  
+});
